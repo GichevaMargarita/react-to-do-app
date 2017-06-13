278b20013335fe96778ac94525e7f476fb0b7d7f
GichevaMargarita <margoshka_love20@mail.ru>
2017-06-13 18:34:41 +0300

    data property was added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
 import React, {Component} from "react";
 import "./App.css";
 
+
 class App extends Component {
+
+
     render() {
+
+        var data = this.props.data;
+        var todoItems = [];
+        var activeItems = [];
+        var completedItems = [];
+
+        if (data.length > 0) {
+            todoItems = data.map(function(item, index) {
+                if(item.isDone){
+                    activeItems.push(<label><input type="checkbox"  checked={!item.isDone}/>{item.name}</label>);
+                }
+                else {
+                    completedItems.push(<label><input type="checkbox" checked={!item.isDone}/>{item.name}</label>);
+                }
+            })
+        } else {
+            todoItems = <p>Nothing to do</p>
+        }
+
         return (
             <div className="css-todo-item">
                 <div className="todo-header">
-                    <h2>Title</h2>
+                    <h2>Title</h2> <span className="minimazeItem">x</span>
                 </div>
                 <div className="todo-list">
                     <div className="active-items">
-                        <label><input type="checkbox"/>Name</label>
-                        <label><input type="checkbox"/>Name</label>
-                        <label><input type="checkbox"/>Name</label>
-                        <label><input type="checkbox"/>Name</label>
-                        <label><input type="checkbox"/>Name</label>
+                        {activeItems}
+                    </div>
+                    <div className="todo-settings">
+                        <span>({data.length}) All ({activeItems.length}) Active ({completedItems.length}) Completed</span>
+                        <span className="showCompleted">V</span>
                     </div>
                     <div className="completed-items">
-                        <label><input type="checkbox"/>Name</label>
-                        <label><input type="checkbox"/>Name</label>
-                        <label><input type="checkbox"/>Name</label>
-                        <label><input type="checkbox"/>Name</label>
+                        {completedItems}
                     </div>
                 </div>
             </div>
--- src/index.js
+++ src/index.js
@@ -3,5 +3,12 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+var data=[{
+    name: "Name 1",
+    isDone: false
+},{
+    name: "Name 2",
+    isDone: true
+}];
+ReactDOM.render(<App data={data}/>, document.getElementById('root'));
 registerServiceWorker();
