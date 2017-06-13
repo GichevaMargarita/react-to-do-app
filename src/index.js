1d8cb82dd9ccdc19eef5a6df05af69744cfc5144
GichevaMargarita <margoshka_love20@mail.ru>
2017-06-13 18:59:25 +0300

    project structure was refactored

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import App from './js/App';
+import registerServiceWorker from './js/registerServiceWorker';
 
 var data=[{
     name: "Name 1",
@@ -10,6 +10,8 @@ var data=[{
     name: "Name 2",
     isActive: false
 }];
+
 ReactDOM.render(<App data={data}/>, document.getElementById('root'));
 // ReactDOM.render(<App data={}/>, document.getElementById('root'));
+
 registerServiceWorker();
