d5ef5770d373ff2795b9c6bf1641af6947069db7
GichevaMargarita <margoshka_love20@mail.ru>
2017-06-19 18:42:51 +0300

    styles for todoSettings were added

--- src/components/ToDoHeader.js
+++ src/components/ToDoHeader.js
@@ -1,20 +1,40 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
+import ToDoSettings from "./ToDoSettings";
 
 class ToDoHeader extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            name: this.props.name ? this.props.name : ''
+            name: this.props.name ? this.props.name : '',
+            isSettingsVisible: false
         };
     }
 
     handleChange(event) {
         const name = event.target.value;
-        this.setState({ name });
+        this.setState({name});
+    }
+
+    triggerSettings() {
+        this.setState({
+            isSettingsVisible: !this.state.isSettingsVisible
+        });
+    }
+
+    clickFunction(){
+
     }
 
     render() {
-        const { name } = this.state;
+        let data = [{
+            settingName: "Name 1",
+            clickFunction: this.clickFunction()
+        }, {
+            settingName: "Name 2",
+            clickFunction: this.clickFunction()
+        }];
+
+        const {name, isSettingsVisible} = this.state;
 
         return (
             <div className="todo-header">
@@ -24,10 +44,12 @@ class ToDoHeader extends Component {
                     value={name}
                     onChange={this.handleChange.bind(this)}
                 />
-                <span />
+                <ToDoSettings data={data} isVisible={isSettingsVisible}/>
+                <span onClick={this.triggerSettings.bind(this)}/>
             </div>
         );
-    };
+    }
+    ;
 }
 
 export default ToDoHeader;
--- src/components/ToDoSettings.js
+++ src/components/ToDoSettings.js
@@ -1,15 +1,19 @@
 import React, {Component} from "react";
+import classNames from 'classnames';
 
 class ToDoSettings extends Component {
     render() {
         let settingsItems = [];
 
-        this.state.settingsItems.map((item, index) => {
-            settingsItems.push(<div key={index} onClick={item.onClick}>{item.name}</div>);
-        });
+        this.props.data.map((item, index) => (
+         settingsItems.push(<div key={index} onClick={item.clickFunction}>{item.settingName}</div>)
+        ));
 
         return (
-            <div className="todo-settings">
+            <div className={classNames({
+                'todo-settings show': this.props.isVisible,
+                'todo-settings hide': !this.props.isVisible,
+            })}>
                 {settingsItems}
             </div>
         );
