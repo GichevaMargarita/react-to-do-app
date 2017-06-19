affdc092c853fb2ba9b4b77cf5e6ef8548ef72d7
GichevaMargarita <margoshka_love20@mail.ru>
2017-06-19 15:25:26 +0300

    attempt to improve scss

--- src/components/RoundButton.js
+++ src/components/RoundButton.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const RoundButton = props => (
-     <div className="todoBtn" onClick={props.onClick}><span/></div>
+     <div className="roundBtn" onClick={props.onClick}><span/></div>
 );
 
 export default RoundButton;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import ToDosContainer from './containers/ToDosContainer';
+import "./scss/main.scss";
 
 ReactDOM.render(<ToDosContainer />, document.getElementById('root'));
